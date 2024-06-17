3d5e1b0fd6fced49adc37cce4ba8ba2a229bc7eb
Jeremy <therealjkjohnson@outlook.com>
2024-06-17 10:59:21 -0700

    Update build-release.sh

--- build/build-release.sh
+++ build/build-release.sh
@@ -7,13 +7,12 @@ source "$SCRIPT_ROOT/common.sh"
 PROJ_ROOT=$(cd -- "${SCRIPT_ROOT}/.." && pwd)
 DATE=$(date +"%Y-%m-%d")
 
-common::checkenv "BUILD_DATE"
 common::checkenv "BUILD_VER"
 
 DOCKER_BUILDKIT=1 docker build --force-rm \
-	--build-arg BUILD_DATE="$BUILD_DATE" \
+	--build-arg BUILD_DATE="$DATE" \
 	--build-arg BUILD_VER="$BUILD_VER" \
-  -f build/Dockerfile "$PROJ_ROOT" \
+	-f build/Dockerfile "$PROJ_ROOT" \
 	-t token_bin:"$BUILD_VER"
 
 docker system prune -f
