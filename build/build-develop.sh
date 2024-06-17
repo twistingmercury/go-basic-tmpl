6b94f80bc353ab099b16a908a5102784b650518f
Jeremy <therealjkjohnson@outlook.com>
2024-06-17 14:07:08 -0700

    Update build-develop.sh

--- build/build-develop.sh
+++ build/build-develop.sh
@@ -7,11 +7,11 @@ source "$SCRIPT_ROOT/common.sh"
 PROJ_ROOT=$(cd -- "${SCRIPT_ROOT}/.." && pwd)
 DATE=$(date +"%Y-%m-%d")
 
-DOCKER_BUILDKIT=1 docker build --force-rm \
+DOCKER_BUILDKIT=1 docker build --force-rm --no-cache \
 --build-arg BUILD_DATE="$DATE" \
 --build-arg BUILD_VER="develop-$DATE" \
 -f build/Dockerfile "$PROJ_ROOT" \
 --target build \
--t token_go_bin:"develop-$DATE"
+-t myapp:"develop-$DATE"
 
 docker system prune -f
